81ac120e71cc8bd01c23632d90076c96c114d204
theone1900 <85264518+theone1900@users.noreply.github.com>
2022-06-07 15:45:46 +0800

    init

--- pkg/localexec/localexec.go
+++ pkg/localexec/localexec.go
@@ -127,14 +127,14 @@ SID_LIST_LISTENER =
 		return string
 	} else {
 		cmds := fmt.Sprintf(`su - %s -c 'echo "
-#LISTENER2 =
-#(DESCRIPTION_LIST =
-#(DESCRIPTION =
-#(ADDRESS = (PROTOCOL = IPC)(KEY = ANYTHING))
-#(ADDRESS = (PROTOCOL = TCP)(HOST = %s)(PORT = 1522))
-#) )
+LISTENER2 =
+(DESCRIPTION_LIST =
+(DESCRIPTION =
+(ADDRESS = (PROTOCOL = TCP)
+(HOST = %s)
+(PORT = 1522) (IP = FIRST))))
 
-SID_LIST_LISTENER =
+SID_LIST_LISTENER2 =
 (SID_LIST =
 (SID_DESC =
 	(GLOBAL_DBNAME = %s)
@@ -164,13 +164,13 @@ func StartListener(cfg service.SourceConfig) (res string, string error) {
 		}
 		return res, err
 	} else {
-		Cmds := fmt.Sprintf(`su - %s %s/bin/lsnrctl start listener`, cfg.StandbyGridHomeOwner, cfg.StandbyGridHome)
-		fmt.Println("[Start local listener]", Cmds)
+		Cmds := fmt.Sprintf(`su - %s %s/bin/lsnrctl start listener2`, cfg.StandbyGridHomeOwner, cfg.StandbyGridHome)
+		fmt.Println("[Start local listener2]", Cmds)
 
 		res, err := RunLocalCommand(Cmds)
 		if err != nil {
 			return res, err
-			log.Info("start standby grid listener failed", zap.String("err", res))
+			log.Info("start standby grid listener2 failed", zap.String("err", res))
 			os.Exit(1)
 		}
 		return res, err
