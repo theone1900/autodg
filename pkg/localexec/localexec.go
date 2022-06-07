c88d96858e2d9ca4b1004c822abf1ff959344b2d
theone1900 <85264518+theone1900@users.noreply.github.com>
2022-06-07 15:09:40 +0800

    init

--- pkg/localexec/localexec.go
+++ pkg/localexec/localexec.go
@@ -127,14 +127,14 @@ SID_LIST_LISTENER =
 		return string
 	} else {
 		cmds := fmt.Sprintf(`su - %s -c 'echo "
-LISTENER =
+LISTENER2 =
 (DESCRIPTION_LIST =
 (DESCRIPTION =
 (ADDRESS = (PROTOCOL = IPC)(KEY = ANYTHING))
-(ADDRESS = (PROTOCOL = TCP)(HOST = %s)(PORT = 1521))
+(ADDRESS = (PROTOCOL = TCP)(HOST = %s)(PORT = 1522))
 ) )
 
-SID_LIST_LISTENER =
+SID_LIST_LISTENER2 =
 (SID_LIST =
 (SID_DESC =
 	(GLOBAL_DBNAME = %s)
@@ -164,8 +164,8 @@ func StartListener(cfg service.SourceConfig) (res string, string error) {
 		}
 		return res, err
 	} else {
-		Cmds := fmt.Sprintf(`su - %s %s/bin/lsnrctl start`, cfg.StandbyGridHomeOwner, cfg.StandbyGridHome)
-		fmt.Println("[Start local listener]", Cmds)
+		Cmds := fmt.Sprintf(`su - %s %s/bin/lsnrctl start listener2`, cfg.StandbyGridHomeOwner, cfg.StandbyGridHome)
+		fmt.Println("[Start local listener2]", Cmds)
 
 		res, err := RunLocalCommand(Cmds)
 		if err != nil {
